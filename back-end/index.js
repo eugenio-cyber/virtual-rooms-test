4f1878ee52e804c7e84d0ffd7d5808bec9b34343
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-20 15:25:29 -0300

    criação do backend

---
+++ back-end/index.js
@@ -0,0 +1,21 @@
+const Koa = require("koa");
+const http = require("http");
+const socket = require("socket.io");
+const cors = require("cors");
+
+const app = new Koa();
+const server = http.createServer(app.callback());
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat.message", (data) => {
+    io.emit("chat.message", data);
+  });
+});
+
+server.listen(8080);
